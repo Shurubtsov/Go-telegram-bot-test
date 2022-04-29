ab4c323b4a10fd17a0d3082c53cee3453a2931cd
Данила Шурубцов <qlyouqwa4ka@gmail.com>
2022-04-29 21:26:44 +0300

    add messages for cmommand

---
+++ events/telegram/messages.go
@@ -0,0 +1,15 @@
+package telegram
+
+const (
+	msgHelp = `I created for save and keep ur pages. I will be 
+	motivate you read them. U can send me some pages and i will 
+	save them to my storage. When u say me i will send them to you and 
+	after command it page will be deleted from my storage. So its motivate for u read them 😊`
+
+	msgHello = "Hi there! \n\n" + msgHelp
+
+	msgUnknownCommand = "Sorry i can't understand you! 😧"
+	msgNoSavedPages   = "My storage is empty, pls send me your pages 🙌"
+	msgSavedPage      = "Saved! 🥳"
+	msgAlreadyExists  = "You already send me that page pls try another 🤝"
+)
